65a62ab6eee6e14287a7b9f48c7b8763571bf1bb
soleilsolis <fukuman23@gmail.com>
2023-10-16 01:08:12 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': 'Outfit, sans-serif',
+    },
+    screens: {
+      'breakpoint': '1207px',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
